ac06a5879e47458961fbc1fcb16d973b704e2d90
ICARUS-OOF <55539292+ICARUS-OOF@users.noreply.github.com>
2025-08-26 15:07:58 +0800

    end credits automated

--- ReplicationSol/Replication/GameStateEnd.cpp
+++ ReplicationSol/Replication/GameStateEnd.cpp
@@ -9,6 +9,8 @@ GameStateEnd::GameStateEnd(GameData* gameData)
 	this->screenPtr = gameData->GetScreenPtr();
 	this->gameStateScreenSize = Vector2(30, 35);
 	currentFrame = 0;
+
+	CreateCredits();
 }
 
 
@@ -22,26 +24,58 @@ void GameStateEnd::OnStateEnter()
 void GameStateEnd::Loop()
 {
 	currentFrame++;
+	if (currentFrame >= creditLines.size())
+		currentFrame = creditLines.size() - 1;
 	Sleep(2000);
 	this->RenderScreen(true);
 }
 
 void GameStateEnd::RenderBaseObjects()
 {
+
 }
 
 void GameStateEnd::RenderBaseUI()
 {
-	if (currentFrame == 0) {
-		screenPtr->RenderText(Vector2(0, 0), "Kayden Chai");
-	}
-	else if (currentFrame == 1) {
-		screenPtr->RenderText(Vector2(0, 0), "Ahmad ");
-		screenPtr->RenderText(Vector2(0, 1), "Kayden Chai");
+	for (int i = currentFrame; i >= 0; i--)
+	{
+		screenPtr->RenderText(Vector2(creditLines[i].GetYOffset(), currentFrame - i), creditLines[i].GetText());
 	}
 }
+
+
+
+void GameStateEnd::CreateCredits()
+{
+	creditLines.push_back(CreditName(10, "Kayden Chai"));
+}
+
+
+
 
 GAMESTATEVALUE GameStateEnd::GetGameStateValue()
 {
 	return GAMESTATEVALUE::ENDSTATE;
+}
+
+
+
+
+
+
+
+GameStateEnd::CreditName::CreditName(int xOffset, std::string text)
+{
+	this->text = text;
+	this->xOffset = xOffset;
+}
+
+std::string GameStateEnd::CreditName::GetText()
+{
+	return text;
+}
+
+int GameStateEnd::CreditName::GetYOffset()
+{
+	return xOffset;
 }
--- ReplicationSol/Replication/GameStateEnd.h
+++ ReplicationSol/Replication/GameStateEnd.h
@@ -3,11 +3,25 @@
 #include "GameData.h"
 #include "GAMESTATEVALUE.h"
 #include "Vector2.h"
+#include <vector>
 
 class GameStateEnd : public GameState
 {
 private:
+	class CreditName {
+	private:
+		std::string text;
+		int xOffset;
+	public:
+		CreditName(int xOffset, std::string text);
+
+		std::string GetText();
+		int GetYOffset();
+	};
+
 	int currentFrame;
+
+	std::vector<CreditName> creditLines;
 
 public:
 	GameStateEnd(GameData* gameData);
@@ -15,6 +29,7 @@ public:
 	void Loop();
 	void RenderBaseObjects();
 	void RenderBaseUI();
+	void CreateCredits();
 	GAMESTATEVALUE GetGameStateValue();
 };
 
